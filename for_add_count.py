2026c21baf1990d4aebd6d19a1747931ff7493fd
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-27 18:38:33 +0000

    Add files via upload

---
+++ for_add_count.py
@@ -0,0 +1,10 @@
+count=int(input("enter how many number do want to add ? "))
+my_sum =0
+for x in range (count):
+    num =int(input("enter the number" + str(x+1)+": " ))
+    my_sum +=num
+
+print(my_sum)
+      
+
+    
